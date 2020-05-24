b35b23fc27e2a1eed924c74b529a73f093eae1ec
Christopher Chaduka <kiri@kiri.co.zw>
2020-05-24 22:50:07 +0200

    Provided solution code, lines 16 & 19

--- Lesson-1-Lambda-functions/wikipedia-query/wikipedia_lambda_exercise.py
+++ Lesson-1-Lambda-functions/wikipedia-query/wikipedia_lambda_exercise.py
@@ -10,22 +10,22 @@ def lambda_handler(event, context):
         :param event: a request with a wikipedia "entity" that has page information
         :return: a response that contains the first sentence of a wikipedia page,
             the response is JSON formatted.'''
-    
+
     ## TO DO: Check that the request has some input body and save it
     if 'body' in event:
-        body = None
-    
+        body = json.loads(event["body"])
+
     ## TO DO: Get the wikipedia "entity" from the body of the request
-    entity = None
+    entity = body["entity"]
     res = wikipedia.summary(entity, sentences=1) # first sentence, result
 
     # print statements
     print(f"context: {context}, event: {event}")
     print(f"Response from wikipedia API: {res}")
-    
+
     ## TO DO: Format the response as JSON and return the result
     response = {
         ## your code here
     }
-    
+
     return response
